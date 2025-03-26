eb0f4e244991ea410b98c2226a679f0d37a369ba
nivahjahshakya <nivahjahshakya@gmail.com>
2025-03-26 10:16:05 +0545

    implement role-based access control for task management

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,31 +1,55 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { JwtAuthGuard } from 'auth/guard/jwt-auth.guard';
+import { RolesGuard } from 'auth/guard/roles.guard';
+import { Roles } from 'auth/decorator/roles.decorator';
+import { Role } from 'auth/enum/role.enum';
 
 @Controller('task')
 export class TaskController {
   constructor(private taskService: TaskService) {}
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Post('create-task')
   createTask(@Body() createTaskDto: CreateTaskDto) {
     return this.taskService.createTask(createTaskDto);
   }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Get('get-all-tasks')
   getAllTasks() {
     return this.taskService.getAllTasks();
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Get('get-task/:id')
   getTask(@Param('id') id: string) {
     return this.taskService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch('update-task/:id')
-  updateTask(@Param('id') id:string, @Body() updateTaskDto: UpdateTaskDto){
-    return this.taskService.updateTask(+id, updateTaskDto)
+  updateTask(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto, @Req() req) {
+    return this.taskService.updateTask(+id, req.user, updateTaskDto);
   }
 
+  @UseGuards(JwtAuthGuard,RolesGuard)
+  @Roles(Role.Admin)
   @Delete('delete-task/:id')
   deleteTask(@Param('id') id: string) {
     return this.taskService.deleteTask(+id);
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,70 +1,78 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Task } from "./entity/task.entity";
-import { Repository } from "typeorm";
-import { CreateTaskDto } from "./dto/create-task.dto";
-import { User } from "auth/entity/user.entity";
-import { UpdateTaskDto } from "./dto/update-task.dto";
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './entity/task.entity';
+import { Repository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { User } from 'auth/entity/user.entity';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { Role } from 'auth/enum/role.enum';
 
 @Injectable()
 export class TaskService {
-    constructor(
-        @InjectRepository(Task)
-        private readonly taskRepository: Repository<Task>,
+  constructor(
+    @InjectRepository(Task)
+    private readonly taskRepository: Repository<Task>,
 
-        @InjectRepository(User)
-        private readonly userRepository: Repository<User>
-    ) {}
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
-    async createTask(createTaskDto: CreateTaskDto): Promise<Task>{
-        const task = new Task();
-        task.title = createTaskDto.title;
-        task.description = createTaskDto.description;
-        if(createTaskDto.user_id){
-            const user = await this.findOne(+createTaskDto.user_id);
-            if(!user){
-                throw new NotFoundException('User not found');
-            }
-            task.user_id = createTaskDto.user_id;
-        }
-        return await this.taskRepository.save(task);
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const task = new Task();
+    task.title = createTaskDto.title;
+    task.description = createTaskDto.description;
+    if (createTaskDto.user_id) {
+      const user = await this.findOne(+createTaskDto.user_id);
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+      task.user_id = createTaskDto.user_id;
     }
-    async findOne(id: number): Promise<Task>{
-        const task = await this.taskRepository.findOne({where: {id}});
-        if(!task){
-            throw new NotFoundException('Task not found')
-        }
-        return task;
+    return await this.taskRepository.save(task);
+  }
+  async findOne(id: number): Promise<Task> {
+    const task = await this.taskRepository.findOne({ where: { id }});
+    if (!task) {
+      throw new NotFoundException('Task not found');
     }
+    return task;
+  }
 
-    async getAllTasks(): Promise<Task[]>{
-        return await this.taskRepository.find({relations: ['user']});
-    }
+  async getAllTasks(): Promise<Task[]> {
+    return await this.taskRepository.find({ relations: ['user'] });
+  }
 
-    async updateTask(id: number, updateTaskDto: UpdateTaskDto){
-        const existingTask = await this.findOne(id);
-        if(!existingTask){
-            throw new NotFoundException('Task not found')
-        }
-        if(updateTaskDto.title) existingTask.title = updateTaskDto.title;
-        if(updateTaskDto.description) existingTask.description = updateTaskDto.description;
-        if(updateTaskDto.status) existingTask.status = updateTaskDto.status;
-        if(updateTaskDto.user_id){
-            const user = this.userRepository.findOne({where: {id: updateTaskDto.user_id}});
-            if(!user){
-                throw new NotFoundException('User not found')
-            }
-            existingTask.user_id = updateTaskDto.user_id;
-        }
-        return this.taskRepository.save(existingTask);
+  async updateTask(id: number, user: User, updateTaskDto: UpdateTaskDto) {
+    const existingTask = await this.findOne(id);
+    if (!existingTask) {
+      throw new NotFoundException('Task not found');
     }
+    const {title, description, user_id, status} = updateTaskDto;
+    if (user.role === Role.Admin) {
+      if (title) existingTask.title = title;
+      if (description)
+        existingTask.description = description;
+      if (user_id) {
+        const _user = this.userRepository.findOne({
+          where: { id: user_id },
+        });
+        if (!_user) {
+          throw new NotFoundException('User not found');
+        }
+        existingTask.user_id = updateTaskDto.user_id;
+      }
+    }else{
+      if(title||description||user_id){
+        throw new NotFoundException('You are not allowed to update title, description, or user_id');
+      }
+    }
+    if (status) existingTask.status = status;
+    return this.taskRepository.save(existingTask);
+  }
 
-    async deleteTask(id: number){
-        const task = await this.findOne(id);
-        if(!task){      
-            throw new NotFoundException('Task not found')
-        }
-        await this.taskRepository.softRemove(task);
-        return 'Task deleted successfully';
-    }
+  async deleteTask(id: number) {
+    const task = await this.findOne(id);
+    await this.taskRepository.softRemove(task);
+    return 'Task deleted successfully';
+  }
 }
